4d921efac0ae8c302a53d4096aa90da4c4bb5967
Nacho Geoghegan <54043072+nachog00@users.noreply.github.com>
2024-08-24 23:13:35 -0300

    added venues data as json

--- src/components/Modal/NewEvent.tsx
+++ src/components/Modal/NewEvent.tsx
@@ -1,9 +1,48 @@
+import { FormControl, FormHelperText, FormLabel, Input, Select, Stack, Text } from "@chakra-ui/react";
 import BaseModal from "./BaseModal";
+import venues from '@/mock/venues';
 
 export default function NewEvent({ children }: { children: React.ReactNode }) {
-  return (
-    <BaseModal title="New Event" target={children}>
-        <p>Modal Content</p>
-    </BaseModal>
-  );
+    return (
+        <BaseModal title="New Event" target={children}>
+            <Stack spacing={4}>
+                <Text>By hosting an event, you have to pick up a venue and decide on the event's prices</Text>
+
+                <FormControl>
+                    <FormLabel>Venue</FormLabel>
+                    <Select placeholder='Select a venue'>
+                        {venues.map(v => {
+                            return <option key={v.id} value={v.id}>{`${v.nombre} - ${v.calle_nombre} ${v.calle_altura}`}</option>
+                        })}
+                    </Select>
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Date</FormLabel>
+                    <Input type="date" placeholder="Date" />
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Time</FormLabel>
+                    <Input type="time" />
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Duration</FormLabel>
+                    <Input type="number" placeholder="Duration" />
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Min Participants</FormLabel>
+                    <Input type="number" placeholder="Min Participants" />
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Max Participants</FormLabel>
+                    <Input type="number" placeholder="Max Participants" />
+                </FormControl>
+                
+            </Stack>
+        </BaseModal>
+    );
 }
---
+++ src/interfaces/Venue.ts
@@ -0,0 +1,20 @@
+export type Venue = {
+    long: string
+    lat: string
+    id: number
+    nombre: string | number
+    categoria: string
+    cocina: string
+    ambientacion: string
+    telefono: string
+    mail: string
+    horario: string
+    calle_nombre: string
+    calle_altura: number
+    calle_cruce: string
+    direccion_completa: string
+    barrio: string
+    comuna: number
+    codigo_postal: number | string
+    codigo_postal_argentino: string
+  }
